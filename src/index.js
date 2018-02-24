6b9956a65662d9d7d403b3ae04686bace3db386f
redgektor <redgektor11@gmail.com>
2018-02-24 22:35:51 +0300

    Brackets implemented

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  var openedBrackets = [];
+	for (var i = 0; i < str.length; i++) {
+		for (var j = 0; j < bracketsConfig.length; j++) {
+			if (bracketsConfig[j][0] == bracketsConfig[j][1] && str[i]==bracketsConfig[j][0]) {
+				if (openedBrackets[openedBrackets.length-1] !== bracketsConfig[j][0]) {
+					openedBrackets.push(str[i]);
+				} else {
+					openedBrackets.pop();
+				}
+
+			} else if (str[i] == bracketsConfig[j][0]) {
+				openedBrackets.push(str[i]);
+			} else if (str[i] == bracketsConfig[j][1]) {
+				if (openedBrackets.length == 0) {
+					return false;
+				} else if (openedBrackets[openedBrackets.length-1] == bracketsConfig[j][0]) {
+					openedBrackets.pop();
+				}
+			}
+		}
+	}
+	if (openedBrackets.length == 0) {
+		return true;
+	} else {
+		return false;
+	}
 }
